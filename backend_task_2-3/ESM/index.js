6df971073cfe95e58c8ce1cd429ab8f0f7dc6896
Davit Mkrtumyan <141499493+davit-06@users.noreply.github.com>
2026-09-01 14:41:39 +0400

    Math-string tasks are pushed

---
+++ backend_task_2-3/ESM/index.js
@@ -0,0 +1,13 @@
+import { add, sub, mul } from './utils/math.js'
+
+import capitalize from './utils/strings.js'
+
+console.log(add(7, 5))
+
+console.log(sub(9, 5))
+
+console.log(mul(5, 5))
+
+console.log(capitalize("vzgo"))
+
+console.log(import.meta.url)
---
+++ backend_task_2-3/cjs/index.js
@@ -0,0 +1,13 @@
+let math = require('./utils/math');
+
+let cap = require('./utils/strings');
+
+console.log(math.add(3, 5))
+
+console.log(math.sub(7, 3))
+
+console.log(math.mul(9, 5))
+
+console.log(cap("bag"))
+
+console.log(require.cache)
